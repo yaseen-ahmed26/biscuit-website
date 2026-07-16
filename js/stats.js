33208166a444f12ccb889d4fab909d5bf90cee9a
yaseen-ahmed26 <yaseen.ahmed26@outlook.com>
2026-07-16 10:54:31 +0100

    fix: format total playtime into hh:mm:ss format

--- js/stats.js
+++ js/stats.js
@@ -8,6 +8,18 @@ const biscuitsLabel = document.getElementById("lifetime-biscuits")
 const playtimeLabel = document.getElementById("lifetime-playtime")
 const clicksLabel = document.getElementById("lifetime-clicks")
 
+function formatPlaytime(totalSeconds){
+    let hours = Math.floor(totalSeconds / 3600);
+    let minutes = Math.floor((totalSeconds % 3600) / 60);
+    let seconds = Math.floor(totalSeconds % 60);
+
+    let paddedHours = String(hours).padStart(2, "0");
+    let paddedMinutes = String(minutes).padStart(2, "0");
+    let paddedSeconds = String(seconds).padStart(2, "0");
+
+    return `${paddedHours}:${paddedMinutes}:${paddedSeconds}`;
+}
+
 function loadSaveData(){
     const userSave = localStorage.getItem("user_save");
 
@@ -18,9 +30,10 @@ function loadSaveData(){
     };
 
     const saveData = JSON.parse(userSave);
+    const formattedTime = formatPlaytime(saveData.total_playtime)
 
     biscuitsLabel.textContent = "Lifetime Biscuits: " + saveData.total_biscuits;
-    playtimeLabel.textContent = "Lifetime PLaytime: " + saveData.total_playtime;
+    playtimeLabel.textContent = "Lifetime PLaytime: " + formattedTime;
     clicksLabel.textContent = "Lifetime Clicks: " + saveData.total_clicks;
 }
 
